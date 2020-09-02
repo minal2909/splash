34546dff74862db1ddf1b243385afc46176c3efa
minal2909 <mistryminal2997@gmail.com>
2020-09-02 14:35:44 +0530

    home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:splash/data/data.dart';
+import 'package:splash/model/categories_model.dart';
+import 'package:splash/model/wallpaper_model.dart';
 import 'package:splash/widgets/widget.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   static String routeName = "/home";
@@ -9,6 +15,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoriesModel> categories = new List();
+  List<WallpaperModel> wallpaper = new List();
+  //
+  // getTrendingWallpapers() async {
+  //   var response = await http.get(
+  //       "https://api.pexels.com/v1/curated?per_page=1",
+  //       headers: {"Authorization": apiKey});
+  //   //print(response.body.toString());
+  //
+  //   Map<String, dynamic> jsonData = jsonDecode(response.body);
+  //   jsonData["photos"].forEach((element) {
+  //     WallpaperModel wallpaperModel = new WallpaperModel();
+  //     wallpaperModel = WallpaperModel.fromMap(element);
+  //     wallpaper.add(wallpaperModel);
+  //   });
+  //   setState(() {});
+  // }
+
+  getTrendingWallpapers() async {
+    var response = await http.get(
+        "https://api.pexels.com/v1/curated?per_page=1",
+        headers: {"Authorization": apiKey});
+
+    //print(response.body.toString());
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      //print(element);
+
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpaper.add(wallpaperModel);
+    });
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getTrendingWallpapers();
+    categories = getCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +67,94 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.white,
         title: AppName(),
         elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                color: Color(0xfff5f8fd),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              margin: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Wallpaper",
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoriesTile(
+                      title: categories[index].catagoriesName,
+                      imgURL: categories[index].imgURL,
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            wallpaperList(wallpaper: wallpaper, context: context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoriesTile extends StatelessWidget {
+  final String imgURL;
+  final String title;
+
+  CategoriesTile({@required this.imgURL, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 5),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              imgURL,
+              height: 50,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 100,
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: Colors.white,
+                  //fontFamily: "mulish",
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
